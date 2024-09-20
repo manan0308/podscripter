dfab45844c16f0a7b7d37bb11c06d7784089f0d2
manan0308 <63553691+manan0308@users.noreply.github.com>
2024-09-20 09:08:01 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    content: [
-      './src/**/*.{js,jsx,ts,tsx}', // Update based on your folder structure
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Will scan everything in src
+    './public/index.html', // Ensure the public index is included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
